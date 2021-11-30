aa80fc1857403d37ecbce7d6158dc38bf267270b
gauravbhatia2 <gauravbhatiagauti@gmail.com>
2021-12-01 03:47:38 +0530

    final commiy

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -18,41 +18,8 @@ readonly APIUrl="http://gauravbhatia-001-site1.itempurl.com/";
   getOrdList():Observable<any[]>{
     return this.http.get<any>(this.APIUrl+'/api/Orders');
   }
-  addCategory(val:any){
-    return this.http.post(this.APIUrl+'/Category',val);
-  }
-
-  updateCategory(val:any){
-    return this.http.put(this.APIUrl+'/Category',val);
-  }
-
-  deleteCategory(val:any){
-    return this.http.delete(this.APIUrl+'/Category/'+val);
-  }
-
-
   getProdList():Observable<any[]>{
     return this.http.get<any>(this.APIUrl+'/api/Products');
   }
 
-  addProducts(val:any){
-    return this.http.post(this.APIUrl+'/Products',val);
-  }
-
-  updateProduts(val:any){
-    return this.http.put(this.APIUrl+'/Products',val);
-  }
-
-  deleteProducts(val:any){
-    return this.http.delete(this.APIUrl+'/Products/'+val);
-  }
-
-
-  UploadPhoto(val:any){
-    return this.http.post(this.APIUrl+'/Products/SaveFile',val);
-  }
-
-  getAllProductsNames():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl+'/Products/GetAllCategoryName');
-  }
 }
